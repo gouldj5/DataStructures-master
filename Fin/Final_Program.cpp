8de0581082d45227610e10401c9136bc1967bb50
gouldj5 <gouldj5@students.rowan.edu>
2017-04-19 15:09:32 -0400

    Better implement code
    Look in idea.txt

--- Fin/Final_Program.cpp
+++ Fin/Final_Program.cpp
@@ -1,5 +1,18 @@
-#include "iostream"
-#include "string"
-#include "cmath"
-
+#include "stdafx.h"
+#include <iostream>     // Provides cout and cin
+#include <cstdlib>      // Provides EXIT_SUCCESS
+#include <string>
+#include "bag3.h"       // probably going to use sorted lists from the bag
+#include "node1.h"
 using namespace std;
+using namespace link;
+
+int main()
+{
+	char name[50];
+	cout << "What is your name?" << endl;
+	cin.getline(name, 50);
+	cout << "You better move fast, " << name << ". The dungeon awaits." << endl;
+	cout << "\n----------------------Press any key to continue----------------------" << endl;
+	return 0;
+}
